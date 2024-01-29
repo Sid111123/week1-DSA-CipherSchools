dbb4cb49c987d92261218fb19238ee957e9ada3e
Sid11123 <praseedsai@gmail.com>
2024-01-29 15:13:10 +0530

    Primitive Data Types

--- Lecture4.java
+++ Lecture4.java
@@ -1,5 +1,21 @@
+import java.util.*;
+
 class Lecture4 {
-    public static void main(String[] args){
-        System.out.println("hello world");
+    public static void main(String ar[]) {
+        int x = 10;
+        int y = 15;
+        System.out.println("X = " + x);
+        System.out.println("Y = " + y);
+        byte b = 12;
+        System.out.println("b = " + b);
+        // Typecasting
+        byte c = (byte) 200;
+        System.out.println("c = " + c);
+
+        int i = c;
+        System.out.println("i = " + i);
+
+        float f = (float) 8.75;
+        System.out.println("f = " + f);
     }
 }
